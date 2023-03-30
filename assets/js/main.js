0536e4afe6a8205e5b5f288a6841b2c3225bff30
AnhTran <letrannhatanh171@gmail.com>
2023-03-30 23:53:49 +0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,61 @@ function runSlide(){
 
 setInterval(runSlide, 1500);
 
+const filterBtn = document.querySelector('.filter-btn');
+const filterModal = document.querySelector('.filter-modal');
+const handleClickFilter = () =>{
+    filterModal.style.display = 'block';
+    filterModal.style.scroll = 'none';
+}
+filterBtn.addEventListener('click', handleClickFilter)
 
+window.onkeydown = function(e) {
+    if ( e.keyCode == 27 ) {
+        filterModal.style.display = 'none';
+    }
+};
+
+const closeModalBtn = document.querySelector('.close-modal-btn i');
+const closeModal = () =>{
+    filterModal.style.display = 'none';
+}
+closeModalBtn.addEventListener('click', closeModal)
+
+const subListBtn = document.querySelectorAll('.title-sidebar');
+const subLists = document.querySelectorAll('.item');
+
+//Xử lí sự kiện onClick vào sidebar
+subListBtn.forEach(function(list, index){
+    var subList = subLists[index];
+    list.addEventListener('click', function(){
+        subList.classList.toggle('show');
+    })
+})
+
+//Xử lý nút menu khi dùng trên điện thoại
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+
+menuBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('show');
+})
+
+//Xử lí nút scroll top
+const searchBtn = document.querySelector('.item-search-btn');
+const goToTopBtn = document.querySelector('.btn-scroll-top');
+
+function scrollShow(){
+    goToTopBtn.addEventListener('click', ()=>{
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+    window.addEventListener('scroll', ()=>{
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopBtn.style.display = "block";
+          } else {
+            goToTopBtn.style.display = "none";
+          }
+    })
+}
+scrollShow();
--- assets/js/product_details.js
+++ assets/js/product_details.js
@@ -34,3 +34,31 @@ function prevImage(){
 nextBtn.addEventListener('click', nextImage);
 //Khi click vào nút prev thì ảnh sẽ quay lại
 prevBtn.addEventListener('click', prevImage);
+
+//Xử lý nút menu khi dùng trên điện thoại
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+
+menuBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('show');
+})
+
+//Xử lí nút scroll top
+const searchBtn = document.querySelector('.item-search-btn');
+const goToTopBtn = document.querySelector('.btn-scroll-top');
+
+function scrollShow(){
+    goToTopBtn.addEventListener('click', ()=>{
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+    window.addEventListener('scroll', ()=>{
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopBtn.style.display = "block";
+          } else {
+            goToTopBtn.style.display = "none";
+          }
+    })
+}
+scrollShow();
